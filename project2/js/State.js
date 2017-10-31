3b0d9b867cf4d43678ece8bfd99e0638bf976ce7
catnoodle <fh6435@126.com>
2017-10-30 23:28:30 -0400

    modify

--- project2/js/State.js
+++ project2/js/State.js
@@ -1702,7 +1702,7 @@ var optionWorldMap = {
     visualMap: {
         left: 'left',
         min: 0,
-        max: 300000,
+        max: 350000,
         inRange: {
             color: ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090', '#fdae61', '#f46d43', '#c73730', '#a50026']
         },
